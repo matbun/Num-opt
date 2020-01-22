5643cb7e6325aaa24b878a59317ddb43a5267135
matbun <matteo.bunino@gmail.com>
2020-01-22 15:14:48 +0100

    ciao

--- p1/code/predictor_corrector.m
+++ p1/code/predictor_corrector.m
@@ -4,6 +4,8 @@ m = size(A,1);
 
 e = ones(n,1);
 
+gamma = 0.9995;
+
 mu = x'*s/n;
 mu_0 = mu;
 
@@ -18,16 +20,16 @@ for k = 0:maxiter
     AA(m+n+1:m+2*n, 1:n) = S;
     AA(m+n+1:m+2*n, m+n+1:m+2*n) = X;
     
-    r_c = s+A'*lambda-c;
-    r_b = A*x-b;
-    r_xs = X*S*e;
+    r_a = -(A*x-b);
+    r_b = -(s+A'*lambda-c);
+    r_c = -X*S*e;
     
     %solve the first system
-    D = (inv(S)*X).^(1/2);
-
-    d_lambda_aff = (A*D.^2*A') \ (-r_b - A*X*inv(S)*r_c + A*inv(S)*r_xs);
-    d_s_aff = -r_c -A'*d_lambda_aff;
-    d_x_aff = -inv(S)*r_xs - X*inv(S)*d_s_aff;
+    M = A*inv(S)*X*A'; %coefficients matrix to compute d_lambda_aff and d_lambda
+    
+    d_lambda_aff = M \ (r_a + A*inv(S)*X*r_b - A*inv(S)*r_c);
+    d_s_aff = r_b - A'*d_lambda_aff;
+    d_x_aff = inv(S)*r_c - inv(S)*X*d_s_aff;
     
     if isempty(d_x_aff(d_x_aff<0))
         alfa_aff_primal = 1;
@@ -44,12 +46,12 @@ for k = 0:maxiter
     mu_aff = (x + alfa_aff_primal*d_x_aff)'*(s + alfa_aff_dual*d_s_aff)/n;
     sigma = (mu_aff/mu)^3;
     
-    r_xs = r_xs + spdiags(d_x_aff,0,n,n)*spdiags(d_s_aff,0,n,n)*e - sigma*mu*e;
+    r_c = r_c - spdiags(d_x_aff,0,n,n)*spdiags(d_s_aff,0,n,n)*e + sigma*mu*e;
     
     %solve the second system
-    d_lambda = (A*D.^2*A') \ (-r_b - A*X*inv(S)*r_c + A*inv(S)*r_xs);
-    d_s = -r_c -A'*d_lambda;
-    d_x = -inv(S)*r_xs - X*inv(S)*d_s;
+    d_lambda = M \ (r_a + A*inv(S)*X*r_b - A*inv(S)*r_c);
+    d_s = r_b - A'*d_lambda;
+    d_x = inv(S)*r_c - inv(S)*X*d_s;
     
     
     alfa_primal_max = min(-x(d_x_aff<0)./d_x_aff(d_x_aff<0));
@@ -69,9 +71,9 @@ for k = 0:maxiter
         alfa_dual = min(1,eta*alfa_dual_max);
     end
     
-    x = x + alfa_primal*d_x;
-    lambda = lambda + alfa_dual*d_lambda;
-    s = s + alfa_dual*d_s;
+    x = x + gamma*alfa_primal*d_x;
+    lambda = lambda + gamma*alfa_dual*d_lambda;
+    s = s + gamma*alfa_dual*d_s;
     
     mu = x'*s/n;
     if mu < epsilon*mu_0
--- p1/code/predictor_corrector_lu.m
+++ p1/code/predictor_corrector_lu.m
@@ -4,6 +4,8 @@ m = size(A,1);
 
 e = ones(n,1);
 
+gamma = 0.9995;
+
 mu = x'*s/n;
 mu_0 = mu;
 
@@ -18,18 +20,19 @@ for k = 0:maxiter
     AA(m+n+1:m+2*n, 1:n) = S;
     AA(m+n+1:m+2*n, m+n+1:m+2*n) = X;
     
-    r_c = s+A'*lambda-c;
-    r_b = A*x-b;
-    r_xs = X*S*e;
+    r_a = -(A*x-b);
+    r_b = -(s+A'*lambda-c);
+    r_c = -X*S*e;
     
-    r = [-r_b; -r_c; -r_xs];
-    [L,U] = lu(AA);
-    y = L\r;
-    r = U\y;
-
+    r = [r_a; r_b; r_c];
+    [L,U,P,Q] = lu(AA);
+    z = L\(P*r);
+    y = U\z;
+    r = Q*y;
+    
+    d_x_aff = r(1:n);
     d_lambda_aff = r(n+1);
     d_s_aff = r(n+2:end);
-    d_x_aff = r(1:n);
     
     if isempty(d_x_aff(d_x_aff<0))
         alfa_aff_primal = 1;
@@ -46,17 +49,17 @@ for k = 0:maxiter
     mu_aff = (x + alfa_aff_primal*d_x_aff)'*(s + alfa_aff_dual*d_s_aff)/n;
     sigma = (mu_aff/mu)^3;
     
-    r_xs = r_xs + spdiags(d_x_aff,0,n,n)*spdiags(d_s_aff,0,n,n)*e - sigma*mu*e;
+    r_c = r_c - spdiags(d_x_aff,0,n,n)*spdiags(d_s_aff,0,n,n)*e + sigma*mu*e;
     
     %solve the second system
-    r = [-r_b; -r_c; -r_xs];
-    y = L\r;
-    r = U\y;
+    r = [r_a; r_b; r_c];
+    z = L\(P*r);
+    y = U\z;
+    r = Q*y;
     
+    d_x = r(1:n);
     d_lambda = r(n+1);
     d_s = r(n+2:end);
-    d_x = r(1:n);
-    
     
     alfa_primal_max = min(-x(d_x_aff<0)./d_x_aff(d_x_aff<0));
     alfa_dual_max = min(-s(d_s_aff<0)./d_s_aff(d_s_aff<0));
@@ -75,9 +78,9 @@ for k = 0:maxiter
         alfa_dual = min(1,eta*alfa_dual_max);
     end
     
-    x = x + alfa_primal*d_x;
-    lambda = lambda + alfa_dual*d_lambda;
-    s = s + alfa_dual*d_s;
+    x = x + gamma*alfa_primal*d_x;
+    lambda = lambda + gamma*alfa_dual*d_lambda;
+    s = s + gamma*alfa_dual*d_s;
     
     mu = x'*s/n;
     if mu < epsilon*mu_0
